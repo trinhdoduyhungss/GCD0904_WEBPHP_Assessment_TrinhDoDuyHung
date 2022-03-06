6191f5018f411d9f0bf79cd4d37279d9a0138794
hungtt <hungtddgcd201514@fpt.edu.vn>
2022-03-06 10:02:25 +0700

    Update code for  Contact page

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\UserInfoRepository;
+use App\Manager\CartManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,25 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact_us")
      */
-    public function index(): Response
+    public function index(CartManager $cartManager, 
+    UserInfoRepository $userInfoRepository): Response
     {
+        $cart = $cartManager->getCurrentCart();
+        $userName = [];
+        if($cart->getId()){
+            $userName = $userInfoRepository->findOneBy(['userid' => $cart->getId()]);
+            if($userName == null){
+                $userName = [];
+                $userName['username'] = 'Guest';
+            }
+        }else{
+            $userName['username'] = 'Guest';
+        }
+        $cart = $cart->getItems()->count() ? $cart : 0;
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+            'cart' => $cart,
+            'userName' => $userName
         ]);
     }
 }
